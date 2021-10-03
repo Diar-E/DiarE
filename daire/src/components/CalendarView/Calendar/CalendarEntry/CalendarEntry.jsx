f052e77f5dc5fb659a1fa2ac1f2f67b0b90c0c4d
3keeksi <46215747+3keeksi@users.noreply.github.com>
2021-10-03 22:54:46 +0200

    Frontend Push
    03.10.2021 Status - Calender Missing

---
+++ daire/src/components/CalendarView/Calendar/CalendarEntry/CalendarEntry.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CalendarEntry.css'
+
+const CalendarEntry = (value) => {
+    return (
+        <div className="calendarEntry">
+            {value}
+        </div>
+    )
+}
+
+export default CalendarEntry
