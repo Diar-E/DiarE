988a8094d52aa041e6937785d6644490d47737eb
3keeksi <46215747+3keeksi@users.noreply.github.com>
2021-12-21 14:03:48 +0100

    FrontEnd
    Login View added
    Routing works

--- client/src/components/LoginView/LoginView.jsx
+++ client/src/components/LoginView/LoginView.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './LoginView.css';
 
 const LoginView = () => {
     return (
-        <div>
-            Login login
+        <>
+        <div className="loginView">
+            <div className="cont">
+                
+                <div className="bigpoint active"></div>
+
+                <div className="bigpoint"></div>
+            
+                <div className="bigpoint"></div>
+            </div>
+
+
+            <label for="username">Username <br></br></label>
+            <input name="username" type="textfield"></input>
+
+            <br></br>
+
+            <label for="nachname">Password <br></br></label>
+            <input name="nachname" type="password"></input>
+
+            <div className="cont">
+                <div className="button back">Previous</div>
+                <div className="button next">Next</div>
+            </div>
         </div>
+        </>
     )
 }
 
